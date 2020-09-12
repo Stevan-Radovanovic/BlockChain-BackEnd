65374b602a4aecbb6621dcafe4bd5333d2901c77
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-09-12 22:52:49 +0200

    [Commit #10] Added console log and error outputs

--- blockchain.js
+++ blockchain.js
@@ -4,6 +4,7 @@ const CryptoHash = require('./crypto-hash');
 class Blockchain {
   constructor() {
     this.chain = [Block.genesis()];
+    console.log('Created a Blockchain');
   }
 
   addBlock({ data }) {
@@ -12,10 +13,12 @@ class Blockchain {
       data,
     });
     this.chain.push(block);
+    console.log('Added a Block to the Chain');
   }
 
   static isValidChain(chain) {
     if (JSON.stringify(Block.genesis()) !== JSON.stringify(chain[0])) {
+      console.error('Validation Error: First Block not a Genesis block');
       return false;
     }
 
@@ -27,18 +30,24 @@ class Blockchain {
         CryptoHash(current.lastHash, current.data, current.timestamp) !==
         current.hash
       )
-        return false;
+        console.error('Validation Error: LastHash = Hash Rule not followed');
+      return false;
     }
 
+    console.log('Chain succesfully validated');
     return true;
   }
 
   replaceChain(newChain) {
     if (this.chain.length >= newChain.length) {
+      console.error(
+        'Replacement Error: New Chain not longer than the previous one'
+      );
       return;
     }
 
     if (Blockchain.isValidChain(newChain)) {
+      console.log('Chain succesfully replaced');
       this.chain = newChain;
     }
   }
