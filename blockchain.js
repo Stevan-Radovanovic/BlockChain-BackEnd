df55383bc839a39d353e3beffc7b3c480ecea6fb
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-09-12 22:26:50 +0200

    [Commit #8] Added chain validation method

--- blockchain.js
+++ blockchain.js
@@ -1,4 +1,5 @@
 const Block = require('./block');
+const CryptoHash = require('./crypto-hash');
 
 class Blockchain {
   constructor() {
@@ -11,6 +12,25 @@ class Blockchain {
       data,
     });
     this.chain.push(block);
+  }
+
+  static isValidChain(chain) {
+    if (JSON.stringify(Block.genesis()) !== JSON.stringify(chain[0])) {
+      return false;
+    }
+
+    for (const index = 1; index < chain.length; index++) {
+      const current = chain[i];
+      const previous = chain[i - 1];
+      if (current.lastHash !== previous.hash) return false;
+      if (
+        CryptoHash(current.lastHash, current.data, current.timestamp) !==
+        current.hash
+      )
+        return false;
+    }
+
+    return true;
   }
 }
 
