78c10acfb977f97fb0765d2b1623c5acf7bc2252
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-09-13 17:50:40 +0200

    [Commit #19] Added POST requests and body-parser

--- src/api/app.js
+++ src/api/app.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain/blockchain');
 
 const app = express();
 const blockchain = new Blockchain();
 
-app.get('/blocks', (req, res) => {
+app.use(bodyParser.json());
+
+app.get('/blocks', (req, res, next) => {
   res.status(200).json({ chain: blockchain.chain });
+});
+
+app.post('/blocks/return', (req, res, next) => {
+  blockchain.addBlock({ data: req.body.data });
+  res.status(201).json({ message: 'Block added', data: req.body.data });
+});
+
+app.post('/blocks/redirect', (req, res, next) => {
+  const block = blockchain.addBlock({ data: req.body.data });
+  res.redirect('/blocks');
 });
 
 console.log('BlockChain API starting');
