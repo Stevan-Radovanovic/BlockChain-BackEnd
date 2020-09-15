26ec7a730176ff5ee1e6ba381f121d33bbc95380
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-09-15 17:18:43 +0200

    [Commit #28] Added Ellipctic for generating keypair values

--- src/crypto/wallet.js
+++ src/crypto/wallet.js
@@ -1,9 +1,10 @@
 const { INITIAL_BALANCE } = require('../../config');
-
+const ec = require('./elliptic');
 class Wallet {
   constructor({ publicKey }) {
     this.balance = INITIAL_BALANCE;
-    this.publicKey = publicKey;
+    const keyPair = ec.genKeyPair();
+    this.publicKey = keyPair.getPublic().encode('hex');
   }
 }
 
