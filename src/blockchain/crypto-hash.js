48262e3c9f356125ee6e7589f91d68bd6fc37e55
stevan_radovanovic <stevan.radovanovic@blinking.id>
2020-12-04 19:30:42 +0100

    Improved crypto hash method

--- src/blockchain/crypto-hash.js
+++ src/blockchain/crypto-hash.js
@@ -2,7 +2,8 @@ const crypto = require('crypto');
 
 const cryptoHash = (...inputs) => {
   const hash = crypto.createHash('sha256');
-  hash.update(inputs.sort().join(' '));
+  
+  hash.update(inputs.sort().map((input) => JSON.stringify(input)).join(' '));
 
   return hash.digest('hex');
 };
